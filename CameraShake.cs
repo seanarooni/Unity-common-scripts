51c4c590a7e578c2b4603047d58daa8120f059f6
Sean Iveson <seanarooni@gmail.com>
2015-02-20 14:51:03 -0500

    Update and rename PerlinNoiseCameraShake.cs to CameraShake.cs
    Camera Shake using perlin noise
    Must be attached to the camera at this time. 
    Called by another script via something like: Camera.main.GetComponent<CameraShake>().PlayShake();

--- CameraShake.cs
+++ CameraShake.cs
@@ -1,36 +1,52 @@
-public class PerlinNoiseCameraShake : MonoBehaviour {
+using UnityEngine;
+using System.Collections;
 
+public class CameraShake : MonoBehaviour {
+	
 	public float duration = 0.5f;
 	public float speed = 1.0f;
 	public float magnitude = 0.01f;
+
+	public bool catchUp = false;
+	public float catchUpSpeed = 10.0f;
+	private float increment;
 	
 	public bool shaking = false;
+	private GameObject subject;
+
+	void Start() {
+		subject = GameObject.FindGameObjectWithTag("Player");
+	}
 	
 	// -------------------------------------------------------------------------
 	public void PlayShake() {
 		
-		StopAllCoroutines();
+		StopCoroutine("Shake");
+		shaking = true;
 		StartCoroutine("Shake");
 	}
 	
 	// -------------------------------------------------------------------------
 	void Update() {
-		if (shaking) {
-			shaking = false;
-			PlayShake();
+		if (increment <= 1 && catchUp) {
+			increment += catchUpSpeed / 100;
+			transform.position = Vector3.Lerp(transform.position, new Vector3(subject.transform.position.x, subject.transform.position.y, transform.position.z), increment);
+		} else {
+			increment = 0;
+			catchUp = false;
 		}
 	}
 	
 	// -------------------------------------------------------------------------
 	IEnumerator Shake() {
-		
+
+
 		float elapsed = 0.0f;
 		
 		Vector3 originalCamPos = Camera.main.transform.position;
-		Vector3 originalCamLocalPos = Camera.main.transform.localPosition;
 		float randomStart = Random.Range(-1000.0f, 1000.0f);
-
-		Vector3 _player = GameObject.FindGameObjectWithTag("Player").transform.position;
+		
+		Vector3 _player = subject.transform.position;
 		
 		while (elapsed < duration) {
 			
@@ -42,14 +58,15 @@ public class PerlinNoiseCameraShake : MonoBehaviour {
 			float damper = 1.0f - Mathf.Clamp(2.0f * percentComplete - 1.0f, 0.0f, 1.0f);
 			
 			float alpha = randomStart + speed * percentComplete;
-
+			
 			float x = Mathf.PerlinNoise(alpha, 0) * 2 - 1;
 			float y = Mathf.PerlinNoise(0, alpha) * 2 -1;
-
-			Vector3 player = GameObject.FindGameObjectWithTag("Player").transform.position;
+			
+			Vector3 player = subject.transform.position;
 			Vector3 playerFollow = player - _player;
 			_player = player;
-
+			print (playerFollow.x + " " + playerFollow.y);
+			
 			x *= magnitude * damper;
 			y *= magnitude * damper;
 			
@@ -57,7 +74,7 @@ public class PerlinNoiseCameraShake : MonoBehaviour {
 			
 			yield return null;
 		}
-		shaking = true;
-		Camera.main.transform.localPosition = originalCamLocalPos;
+
+		catchUp = true;
 	}
 }
