e0282ed27b859470cc04ca5961e384e853ed41f1
Sean Iveson <seanarooni@gmail.com>
2015-07-14 20:38:22 -0400

    Create Tags.cs
    example of Tags script.  Allows autocompletion of Tags in mono-develop with Tags.gameController instead of "GameController", etc.

---
+++ Tags.cs
@@ -0,0 +1,8 @@
+public class Tags {
+
+	public const string gameController = "GameController";
+	public const string player = "Player";
+	public const string platform = "Platform";
+	public const string button = "button";
+	public const string pickup = "Pickup";
+}
