5aefa2a3ae020a8fd8a76cff2bdd63bb2cb79b32
Sean Iveson <seanarooni@gmail.com>
2015-07-21 16:03:19 -0400

    Added Comments

--- Tags.cs
+++ Tags.cs
@@ -1,5 +1,12 @@
-public class Tags {
+/*
+* An example of the Tags class I use in all my projects
+* Takes advantage of autocomplete to prevent typos. 
+* Reference in other scripts with Tags.example.
+*/
 
+public class Tags
+{
+	public const string example = "Example";
 	public const string gameController = "GameController";
 	public const string player = "Player";
 	public const string platform = "Platform";
