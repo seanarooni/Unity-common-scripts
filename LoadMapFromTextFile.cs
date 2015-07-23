e21c3cec19f8cd59a672d32d6515b121a9031261
Sean Iveson <seanarooni@gmail.com>
2015-07-23 11:51:44 -0400

    Update LoadMapFromTextFile.cs

--- LoadMapFromTextFile.cs
+++ LoadMapFromTextFile.cs
@@ -2,54 +2,120 @@ using UnityEngine;
 using System.Collections;
 using System.IO;
 
-public class LoadMapFromTextFile : MonoBehaviour {
+public class LoadMaps : MonoBehaviour {
 
-	private GameObject tile01; 
-	private TextAsset MapText;
-	private string mapString;
-
-//origin corresponds to the top left corner of the screen
-//these correspond to default 2D settings with 8 bit tiles (@ 8 pixels per unit)
-	private float originX = -7.5f;
-	private float originY = 4.5f;
-
-	public TextAsset Map01Text; //set in the unity editor 
-
-	// Use this for initialization
-	void Start () {
-		tile01 = Resources.Load("Tile01") as GameObject;
-		Read(Map01Text);
-	}
-
-	void Read(TextAsset file)
-	{
-
+	public Vector2 GetMapSize(TextAsset file)
+	{ //returns the size of the map as a Vector 2, needs to be converted (shown below).
 		StringReader reader = new StringReader(file.text);
+
+		int xSize = 0;
+		int ySize = 0;
 
 		if (reader == null)
 		{
-			print ("Read failed");
-		} 
-		else
+			print ("read failed");
+		}
+		else 
 		{
-			int row = 0;
 			for (string line = reader.ReadLine(); line != null; line = reader.ReadLine())
 			{
-				PrintMapLine(line, row);
-				row--; 
+				if (xSize == 0)
+				{
+					foreach (char c in line)
+					{
+						xSize++;
+					}
+				}
+				ySize++;
 			}
 		}
-
-
+		return new Vector2(xSize, ySize);
 	}
 
-	void PrintMapLine(string line, int row)
-	{
-		for (int i=0; i<line.Length; i++)
+	public TileType[,] Read(TextAsset file) 
+	{//option to call Read without size information.
+		Vector2 size = GetMapSize(file);
+		//convert Vector2 from GetMapSize into integers
+		return Read(file, (int) size.x, (int) size.y); 
+	}
+
+	public TileType[,] Read(TextAsset file, int xSize, int ySize) 
+	{ 
+		StringReader reader = new StringReader(file.text);
+
+		TileType[,] ttmap = new TileType[xSize, ySize];
+
+		if (reader == null)
 		{
-			if (line[i].ToString() == "1")
+			print ("read failed");
+			return null;
+		} 
+		else 
+		{
+			int x = 0; 
+			int y = ySize - 1;
+			for (string line = reader.ReadLine(); line != null; line = reader.ReadLine())
 			{
-				Instantiate(tile01, new Vector2(i + originX, row + originY), Quaternion.identity);
+				//PrintMapLine(line, row);
+				x = 0;
+				foreach(char c in line)
+				{
+					//print ("" + x + " " + y);
+					ttmap[x,y] = ConvertToTileTypeFromChar(c, x, y);
+					x++;
+				}
+
+				y--;
 			}
+			return ttmap;
 		}
 	}
+
+	/*----------------------------------------------------------------------------
+	 * This method is an example of one way to process the char data into your game:
+	 * This mess of a method converts the individual characters from the text file 
+	 * into BlockTypes tType enums from the TileType class.  
+	 * Most of the remaining properties (movementCost, visualPrefab) of the newly 
+	 * constructed TileTypes are defined in TileMap's SetUpMap().
+	 * the inUse property is set to false in the constructors run by the function.*/
+	TileType ConvertToTileTypeFromChar(char c, int x, int y)
+	{ 
+		TileType tt;
+
+		switch(c)
+		{
+			
+		case '0': //normal
+			tt = new TileType(TileType.BlockTypes.Dirt);
+			break;
+		case '1': //stone 
+			tt = new TileType(TileType.BlockTypes.Edge); //impassable rock
+			break;
+		case '2': //filled in space
+			tt = new TileType(TileType.BlockTypes.Filled); //impassable except by player
+			break;
+		case 'e':
+			tt = new TileType(TileType.BlockTypes.Dirt);
+			Instantiate(Resources.Load("Enemy") as GameObject, new Vector2(x, y), Quaternion.identity);
+			break;
+		case 'p':
+			tt = new TileType(TileType.BlockTypes.Dirt);
+			Instantiate(Resources.Load("Player") as GameObject, new Vector2(x, y), Quaternion.identity);
+			break;
+		default:
+			tt = new TileType(TileType.BlockTypes.Dirt);
+			break;
+		}
+	
+		return tt;
+	}
+	
+	/*this method instantiates tiles passed from TileMap's GenerateVisual function,
+	* then sets its parent in the Unity Hierarcy to the GameController. 
+	* Consider adding these GOs to an object pool for more efficient reuse later.	*/
+	public void CreateTile(TileType str, int x, int y)
+	{  
+		GameObject tt = Instantiate(str.visualPrefab, new Vector2(x,y), Quaternion.identity) as GameObject;
+		tt.transform.SetParent(GameObject.FindGameObjectWithTag(Tags.gameController).transform);
+	}
+}
