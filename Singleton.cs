bbf324dccba4f2596796ebb2703e9786fef29138
Sean Iveson <seanarooni@gmail.com>
2015-08-14 23:24:08 -0400

    Update Singleton.cs

--- Singleton.cs
+++ Singleton.cs
@@ -12,7 +12,7 @@ public class Singleton : MonoBehaviour
   {
     if (instance == null) 
     {
-      instance == this;
+      instance = this;
     } else if (instance != this) {
       Destroy(gameObject);
     }
